eb9edaffe7b396c71fc141dd1f461a5fee9fb0d1
ijosip <ivanjosipovicskola@gmail.com>
2026-01-12 04:09:37 +0100

    Update Student.h

--- Vjezba7/Student.h
+++ Vjezba7/Student.h
@@ -330,33 +330,3 @@ void StudentOffice::update_student_years(std::vector<Student>& students)
 }
 ```
 
-# main.cpp
-
-```cpp
-#include <vector>
-#include "Student.h"
-#include "StudentOffice.h"
-#include "UniversityConstants.h"
-
-int main()
-{
-    UniversityConstants::print_university_rules();
-
-    Student s1(1, "Ana", "CS", 1);
-    Student s2(2, "Marko", "Math", 1);
-
-    std::vector<Student> students{ s1, s2 };
-
-    Course c1("OOP", "CS101", 5);
-    Course c2("ALG", "CS102", 5);
-
-    StudentOffice::enroll_student(students[0], c1);
-    StudentOffice::process_exam_results(students[0], c1);
-    StudentOffice::update_student_years(students);
-
-    for (const auto& s : students)
-        std::cout << s << "\n";
-
-    std::cout << "Total students: " << Student::get_total_students() << "\n";
-}
-```
