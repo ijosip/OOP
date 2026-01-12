0232907e8a031638b42d8c7cb75c9f730297000a
ijosip <ivanjosipovicskola@gmail.com>
2026-01-12 04:12:15 +0100

    Update Student.h

--- Vjezba7/Student.h
+++ Vjezba7/Student.h
@@ -231,23 +231,6 @@ public:
 #endif
 ```
 
-# Course.cpp
-
-```cpp
-#include "Course.h"
-
-std::ostream& operator<<(std::ostream& os, const Course& c)
-{
-    os << c.name << " (" << c.code << ", " << c.ects << " ECTS)";
-    return os;
-}
-
-std::istream& operator>>(std::istream& is, Course& c)
-{
-    is >> c.name >> c.code >> c.ects;
-    return is;
-}
-```
 
 
 
