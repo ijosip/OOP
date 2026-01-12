df0321e3d3960a896e552f5738fe393dc839d3b1
ijosip <ivanjosipovicskola@gmail.com>
2026-01-12 04:18:05 +0100

    Create Player.h

---
+++ Vjezba8/Player.h
@@ -0,0 +1,26 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+#include "GameCharacter.h"
+
+
+class Enemy;
+
+
+class Player : public GameCharacter {
+protected:
+int score;
+
+
+public:
+Player(const std::string& name, int health);
+virtual ~Player();
+
+
+virtual void attackEnemy(Enemy* target) = 0;
+void addScore(int amount);
+};
+
+
+#endif
