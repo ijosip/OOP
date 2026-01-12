4a10df0a04c751093def8013851a4f2a5ebf53a5
ijosip <ivanjosipovicskola@gmail.com>
2026-01-12 04:20:26 +0100

    Create Gnome.cpp

---
+++ Vjezba8/Gnome.cpp
@@ -0,0 +1,26 @@
+#include "Gnome.h"
+#include "Player.h"
+
+
+Gnome::Gnome(const std::string& name)
+: Enemy(name, 90, 3) {}
+
+
+void Gnome::attackPlayer(Player* target)
+{
+if (!target || !target->isAlive()) return;
+target->takeDamage(5 * difficulty);
+}
+
+
+void Gnome::specialAbility()
+{
+if (health > 20)
+std::cout << name << " blows the trumpet for help!" << std::endl;
+}
+
+
+void Gnome::displayStatus() const
+{
+std::cout << "Gnome " << name << " | HP: " << health << " | Difficulty: " << difficulty << std::endl;
+}
