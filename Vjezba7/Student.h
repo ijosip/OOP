99d87301e8d29aa98dc695c173fd43d9c7da90aa
ijosip <ivanjosipovicskola@gmail.com>
2026-01-12 04:11:47 +0100

    Update Student.h

--- Vjezba7/Student.h
+++ Vjezba7/Student.h
@@ -249,27 +249,6 @@ std::istream& operator>>(std::istream& is, Course& c)
 }
 ```
 
-# UniversityConstants.h
-
-```cpp
-#ifndef UNIVERSITY_CONSTANTS_H
-#define UNIVERSITY_CONSTANTS_H
-
-#include <iostream>
-
-struct UniversityConstants {
-    static const int MAX_ETCS_PER_YEAR = 60;
-    static const int REQUIRED_ECTS_PER_YEAR = 45;
-
-    static void print_university_rules()
-    {
-        std::cout << "MAX_ETCS_PER_YEAR=" << MAX_ETCS_PER_YEAR << "\n";
-        std::cout << "REQUIRED_ECTS_PER_YEAR=" << REQUIRED_ECTS_PER_YEAR << "\n";
-    }
-};
-
-#endif
-```
 
 
 
