2fb873b865eb6c96c8e09eeec96890ef98adfe34
ijosip <ivanjosipovicskola@gmail.com>
2026-01-12 04:11:13 +0100

    Update Student.h

--- Vjezba7/Student.h
+++ Vjezba7/Student.h
@@ -271,24 +271,5 @@ struct UniversityConstants {
 #endif
 ```
 
-# StudentOffice.h
-
-```cpp
-#ifndef STUDENT_OFFICE_H
-#define STUDENT_OFFICE_H
-
-#include <vector>
-#include "Student.h"
-
-class StudentOffice {
-public:
-    static void moveStudent(Student& s, std::string new_program);
-    static void enroll_student(Student& s, const Course& c);
-    static void process_exam_results(Student& s, const Course& c);
-    static void update_student_years(std::vector<Student>& students);
-};
-
-#endif
-```
 
 
