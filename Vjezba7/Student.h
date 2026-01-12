2896465f20dfd4a1877e81c9e6f65c60f71fb2b0
ijosip <ivanjosipovicskola@gmail.com>
2026-01-12 04:12:51 +0100

    Update Student.h

--- Vjezba7/Student.h
+++ Vjezba7/Student.h
@@ -203,33 +203,7 @@ std::istream& operator>>(std::istream& is, Student& s)
 }
 ```
 
-# Course.h
 
-```cpp
-#ifndef COURSE_H
-#define COURSE_H
-
-#include <string>
-#include <iostream>
-
-class Course {
-private:
-    std::string name;
-    std::string code;
-    int ects;
-
-public:
-    Course(const std::string& n = "", const std::string& c = "", int e = 5)
-        : name(n), code(c), ects(e) {}
-
-    int get_ects() const { return ects; }
-
-    friend std::ostream& operator<<(std::ostream&, const Course&);
-    friend std::istream& operator>>(std::istream&, Course&);
-};
-
-#endif
-```
 
 
 
