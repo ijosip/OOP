e67da4450a6842fcb9c3727a0333c4706002384c
ijosip <ivanjosipovicskola@gmail.com>
2026-01-12 04:19:43 +0100

    Create Mage.h

---
+++ Vjezba8/Mage.h
@@ -0,0 +1,21 @@
+#ifndef MAGE_H
+#define MAGE_H
+
+
+#include "Player.h"
+
+
+class Mage : public Player {
+private:
+int mana;
+
+
+public:
+Mage(const std::string& name);
+void attackEnemy(Enemy* target) override;
+void specialAbility() override;
+void displayStatus() const override;
+};
+
+
+#endif
